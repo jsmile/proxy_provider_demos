253b20b277837ec2d5e7ed0c3bdeaa1f0eeebbc2
jsmile <jsmile@naver.com>
2023-11-04 22:14:27 +0900

    타 class 를 사용하기 위한 Provider() 위젯

--- lib/src/pages/why_porxyprov.dart
+++ lib/src/pages/why_porxyprov.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+/// Why ProxyProvider
+///
+/// 어떤 위젯이 다른 class 에 의존하고 있을 때,
+///
+/// 1. Provider 위젯 사용
+/// 해당 class 를 사용할 수 있게 상위에서 Provider 위젯의 create 로 생성하고,
+/// child 에서 Provider.of<T>() 로 접근하여 사용할 수 있게 함.
+/// but
+/// create 는 초기 1회만 실행되므로 그 후 변경되는 상태를 반영하지 못함.
+
+// ShowTranslation 에서 의존하는 class ( data class)
 class Translations {
   final int _value;
 
@@ -30,30 +41,40 @@ class _WhyProxyProvState extends State<WhyProxyProv> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Why ProxyProvider'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const ShowTransrations(),
-            const SizedBox(height: 20),
-            IncreasedButton(increment: increment),
-          ],
+        // 관련 데이터( translations )를 하위에서 사용하게 하기 위하여
+        // 해당 유형을 생성할 수 있는 Provider 로 감싸줌
+        child: Provider<Translations>(
+          create: (_) => Translations(counter), // 사용할 의존데이터 생성
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const ShowTransrations(),
+              const SizedBox(height: 20),
+              IncreasedButton(increment: increment),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
+// Translations 의 instance 를 구해서 접근할 수 있으면
+// property 를 title 을 통해 접근할 수 있고,
+// 그러면 해당 State( data )의 변화를 표시할 수 있음.
 class ShowTransrations extends StatelessWidget {
   const ShowTransrations({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'You clicked 0 times!',
-      style: TextStyle(fontSize: 28.0),
+    final title = Provider.of<Translations>(context).title;
+    return Text(
+      title,
+      style: const TextStyle(fontSize: 28.0),
     );
   }
 }
